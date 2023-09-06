f3d56850c5462e827e0a0c9eb8b462e0b43e1a37
Dmitry-Krylov <93715663+Dmitry-Krylov@users.noreply.github.com>
2023-09-06 17:44:26 +0200

    06.09.2023

--- SearchMusic/Models/SearchResponse.swift
+++ SearchMusic/Models/SearchResponse.swift
@@ -14,8 +14,8 @@ struct SearchResponse: Decodable {
 }
 
 struct Track: Decodable {
-    var trackName: String
-    var collectionName: String
+    var trackName: String?
+    var collectionName: String?
     var artistName: String
     var artworkUrl60: String?
 }
--- SearchMusic/ViewController.swift
+++ SearchMusic/ViewController.swift
@@ -9,10 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let networkService = NetworkService()
+    let networkDataFetcher = NetworkDataFetcher()
+    var searchResponse: SearchResponse? = nil
+    private var timer: Timer?
+    let searchController = UISearchController(searchResultsController: nil)
     
     @IBOutlet weak var table: UITableView!
-    let searchController = UISearchController(searchResultsController: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,32 +22,13 @@ class ViewController: UIViewController {
         setupTabelView()
         setupSearchBar()
         self.table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        
-        let urlString = "https://itunes.apple.com/search?term=jack+johnson&limit=5"
-//        request(urlString: urlString) { (searchResponse, error) in
-//            searchResponse?.results.map({ (track) in
-//                print(track.trackName)
-//            })
-//        }
-        networkService.request(urlString: urlString) { (result) in
-            switch result {
-            case .success(let searchResponse):
-                searchResponse.results.map { (track) in
-                    print("track.trackName:", track.trackName)
-                }
-            case .failure(let error):
-                print("error", error)
-            }
-        }
     }
-
 
     private func setupSearchBar() {
         navigationItem.searchController = searchController
         searchController.searchBar.delegate = self
         navigationController?.navigationBar.prefersLargeTitles = true
         searchController.obscuresBackgroundDuringPresentation = false
-        
     }
     
     private func setupTabelView() {
@@ -53,24 +36,37 @@ class ViewController: UIViewController {
         table.dataSource = self
         table.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
-
 }
 
+
+//MARK: UITableViewDelegate, UITableViewDataSource
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return searchResponse?.results.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Hola"
+        let track = searchResponse?.results[indexPath.row]
+        print("track?.artworkUrl60:", track?.artworkUrl60)
+        cell.textLabel?.text = track?.trackName
         return cell
     }
 }
 
+//MARK: UISearchBarDelegate
 extension ViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        print(searchText)
+        let urlString = "https://itunes.apple.com/search?term=\(searchText)&limit=10"
+        
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { _ in
+            self.networkDataFetcher.fetchTracks(urlString: urlString) { (searchResponse) in
+                guard let searchResponse = searchResponse else { return }
+                self.searchResponse = searchResponse
+                self.table.reloadData()
+            }
+        })
     }
 }
